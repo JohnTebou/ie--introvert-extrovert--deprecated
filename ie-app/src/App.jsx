114671c3c229487c6645cf16193415f159cd9927
JohnTebou <johntebou@gmail.com>
2024-10-26 11:01:49 -0400

    file rename issue

--- ie-app/src/App.jsx
+++ ie-app/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 
+import Navbar from './components/Navbar.jsx'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Navbar>
 
+      </Navbar>
     </>
   )
 }
